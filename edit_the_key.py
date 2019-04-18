cce8979957f3787c9b72a894d2bb8a24adb9f5f8
amirkhan1092 <amirkhan1092@gmail.com>
2019-04-18 23:13:47 +0530

    dictionary operation

--- edit_the_key.py
+++ edit_the_key.py
@@ -29,5 +29,39 @@ a = dict(b)
 
 # for i in b:
 #     print(a[i],end = ' ')
-
+# {'CAR': ['alto', 'nano'], 'MODEL': 2019, 'SPEED': 110}
 print(a)
+
+
+
+# clear all data from dictionary
+d = {'CAR': ['alto', 'nano'], 'SPEED': 110, 'MODEL': 2019}
+
+d.clear()
+
+print(d)
+
+
+
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+h = d.fromkeys(a,'add')
+h.setdefault('21','Amir')
+# k = h.popitem()
+print(h)
+
+'''
+Python Dictionary Comprehension
+Dictionary comprehension consists of an expression pair (key: value) 
+followed by for statement inside curly braces {}.
+'''
+
+# k = [i for i in range(10)]
+
+sq1 = {x: x*x for x in range(10)}
+sq2 = {x: None for x in range(10)}
+
+# Output: {0: 0, 1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81}
+print(sq1)
+
+
